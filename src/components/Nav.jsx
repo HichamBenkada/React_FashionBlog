0292dfe421985539ef64461a7d98b3b3efff2131
HichamBENKADA <hicham20014@gmail.com>
2024-01-30 17:59:56 -0500

    Nav component has changed to handle links properly

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,12 @@ export default function Nav(props){
 return(
     <nav aria-label="Main Navigation" role="navigation">
           <ul>
-            <li><a href="...">Women's</a></li>
-            <li><a href="...">Men's</a></li>
-            <li><a href="...">On the Street</a></li>
-            <li><a href="...">The Catwalk</a></li>
-            <li><a href="...">AdWatch</a></li>
-            <li><a href="...">About</a></li>
+            <li><a href="#">Women's</a></li>
+            <li><a href="#">Men's</a></li>
+            <li><a href="#">On the Street</a></li>
+            <li><a href="#">The Catwalk</a></li>
+            <li><a href="#">AdWatch</a></li>
+            <li><a href="#">About</a></li>
           </ul>
         </nav>
 )
